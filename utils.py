5c37838c2f8754a2ebf7be174ef601c3552be591
gaerom <rlatofha3597@gmail.com>
2024-10-21 18:02:54 +0900

    add SoundSpacesEvaluator

--- utils.py
+++ utils.py
@@ -1,13 +1,148 @@
+import os 
+import numpy as np
+import pyroomacoustics
+from scipy.signal import hilbert
 import librosa
 import argparse
-import numpy as np
 from math import pi
 from numpy import linalg as LA
-from scipy.signal import hilbert
 import einops
 
 import torch
 import torch.nn.functional as F
+import torchaudio
+import torch.nn as nn
+
+
+################################################### NeRAF #########################################################
+class SpectralLoss(nn.Module):
+    """
+    Compute a loss between two log power-spectrograms. 
+    From  https://github.com/facebookresearch/SING/blob/main/sing/dsp.py#L79 modified
+
+    Arguments:
+        base_loss (function): loss used to compare the log power-spectrograms.
+            For instance :func:`F.mse_loss`
+        epsilon (float): offset for the log, i.e. `log(epsilon + ...)`
+        **kwargs (dict): see :class:`STFT`
+    """
+
+    def __init__(self, base_loss=F.mse_loss, reduction='mean', epsilon=1, dB=False, stft_input_type='mag', **kwargs):
+        super(SpectralLoss, self).__init__()
+        self.base_loss = base_loss
+        self.epsilon = epsilon
+        self.dB = dB
+        self.stft_input_type = stft_input_type
+        self.reduction = reduction
+
+        self.img2mse = lambda x, y : torch.mean((x - y) ** 2)
+        self.mse2psnr = lambda x : -10. * torch.log(x) / torch.log(torch.Tensor([10.]))
+
+    def _log_spectrogram(self, STFT):
+        if self.dB and self.stft_input_type == 'mag':
+            return 10*torch.log10(self.epsilon + STFT) 
+        elif not self.dB and self.stft_input_type == 'mag':
+            return torch.log(self.epsilon + STFT)
+        elif self.stft_input_type == 'log mag': 
+            return STFT
+
+    def forward(self, a, b):
+        spec_a = self._log_spectrogram(a)
+        spec_b = self._log_spectrogram(b)
+        return self.base_loss(spec_a, spec_b, reduction=self.reduction)
+    
+
+def compute_t60(true_in, gen_in, fs, advanced = False):
+    ch = true_in.shape[0]
+    gt = []
+    pred = []
+    for c in range(ch):
+        try:
+            if advanced: 
+                true = measure_rt60_advance(true_in[c], sr=fs)
+                gen = measure_rt60_advance(gen_in[c], sr=fs)
+            else:
+                true = pyroomacoustics.experimental.measure_rt60(true_in[c], fs=fs, decay_db=30)
+                gen = pyroomacoustics.experimental.measure_rt60(gen_in[c], fs=fs, decay_db=30)
+        except:
+            true = -1
+            gen = -1
+        gt.append(true)
+        pred.append(gen)
+    return np.array(gt), np.array(pred)
+
+def measure_rt60_advance(signal, sr, decay_db=10, cutoff_freq=200):
+    # following RAF implementation
+    signal = torch.from_numpy(signal)
+    signal = torchaudio.functional.highpass_biquad(
+        waveform=signal,
+        sample_rate=sr,
+        cutoff_freq=cutoff_freq
+    )
+    signal = signal.cpu().numpy()
+    rt60 = pyroomacoustics.experimental.measure_rt60(signal, sr, decay_db=decay_db, plot=False)
+    return rt60
+
+def measure_clarity(signal, time=50, fs=44100):
+    h2 = signal**2
+    t = int((time/1000)*fs + 1) 
+    return 10*np.log10(np.sum(h2[:t])/np.sum(h2[t:]))
+
+def evaluate_clarity(pred_ir, gt_ir, fs):
+    np_pred_ir = pred_ir
+    np_gt_ir = gt_ir
+
+    # manage multiple channels IR
+    ch = gt_ir.shape[0]
+    gt = []
+    pred = []
+    for c in range(ch):
+        pred_clarity = measure_clarity(np_pred_ir[c,...], fs=fs)
+        gt_clarity = measure_clarity(np_gt_ir[c,...], fs=fs)
+        gt.append(gt_clarity)
+        pred.append(pred_clarity)
+    return np.array(gt), np.array(pred)
+
+def measure_edt(h, fs=44100, decay_db=10):
+    h = np.array(h)
+    fs = float(fs)
+
+    # The power of the impulse response in dB
+    power = h ** 2
+    energy = np.cumsum(power[::-1])[::-1]  # Integration according to Schroeder
+
+    # remove the possibly all zero tail
+    if np.all(energy == 0):
+        return np.nan
+
+    i_nz = np.max(np.where(energy > 0)[0])
+    energy = energy[:i_nz]
+    energy_db = 10 * np.log10(energy)
+    energy_db -= energy_db[0]
+
+    i_decay = np.min(np.where(- decay_db - energy_db > 0)[0])
+    t_decay = i_decay / fs
+    # compute the decay time
+    decay_time = t_decay
+    est_edt = (60 / decay_db) * decay_time 
+    return est_edt
+
+def evaluate_edt(pred_ir, gt_ir, fs):
+    np_pred_ir = pred_ir
+    np_gt_ir = gt_ir
+
+    # manage multiple channels IR
+    ch = gt_ir.shape[0]
+    gt = []
+    pred = []
+    for c in range(ch):
+        pred_edt = measure_edt(np_pred_ir[c], fs=fs)
+        gt_edt = measure_edt(np_gt_ir[c], fs=fs)
+        gt.append(gt_edt)
+        pred.append(pred_edt)
+    return np.array(gt), np.array(pred)
+    
+################################################### NeRAF #########################################################
 
 def energy_decay(pred_mag, gt_mag):
     # [B, T, F]
@@ -204,3 +339,66 @@ def myhibert(x, axis=1):
 
     # Apply transform
     return torch.as_tensor(ht, device=ht.device, dtype=ht.dtype)
+
+
+""" Sound Spaces Evaluator """
+class SoundSpacesEvaluator(object):
+    def __init__(self, fs=22050):
+        self.fs = fs # sample rate
+    
+    def get_full_metrics(self, mag_prd, mag_gt, wav_gt_ff, wav_pred_istft, wav_gt_istft,  log_prd, log_gt):#, gl=False):
+        
+        wav_prd = wav_pred_istft
+        wav_gt = wav_gt_istft
+        n_ch = wav_gt.shape[0]
+
+        # Zero pad waveform to be the same size as gt ff (i.e., max_len * hop_len )
+        wav_prd = np.pad(wav_prd, ((0,0),(0, wav_gt_ff.shape[1]-wav_prd.shape[1])), 'constant', constant_values=(0,0))
+        wav_gt = np.pad(wav_gt, ((0,0),(0, wav_gt_ff.shape[1]-wav_gt.shape[1])), 'constant', constant_values=(0,0))
+
+        ## Waveform related
+        # Compute t60 error, edt and c50 on gt from file
+        t60s_gt, t60s_prd = compute_t60(wav_gt_ff, wav_prd, fs=self.fs) # utils.py
+        t60s = np.concatenate((t60s_gt, t60s_prd))
+        t60s = np.expand_dims(t60s, axis=0)
+        diff = np.abs(t60s[:,n_ch:]-t60s[:,:n_ch])/np.abs(t60s[:,:n_ch])
+        mask = np.any(t60s<-0.5, axis=1)
+        diff = np.mean(diff, axis=1)
+        diff[mask] = 1
+        mean_t60error = np.mean(diff)*100
+        invalid = np.sum(mask)
+        
+        edt_gt, edt_prd = evaluate_edt(wav_prd, wav_gt_ff, fs=self.fs)
+        edts = np.concatenate((edt_gt, edt_prd))
+        edt_instance = np.abs(edts[n_ch:]-edts[:n_ch]) # pred-gt
+        mean_edt = np.mean(edt_instance, axis=0) # mean over instance channels
+
+        c50_gt, c50_prd = evaluate_clarity(wav_prd, wav_gt_ff, fs=self.fs)
+        c50s = np.concatenate((c50_gt, c50_prd))
+        c50_instance = np.abs(c50s[n_ch:]-c50s[:n_ch]) # pred-gt
+        mean_c50 = np.mean(c50_instance, axis=0) # mean over instance channels
+
+        res = {
+                "audio_T60_mean_error": mean_t60error,
+                "audio_total_invalids_T60": invalid,
+                "audio_EDT": mean_edt,
+                "audio_C50": mean_c50,
+                }
+        
+        for key in res.keys():
+            #if tensor go to numpy
+            if isinstance(res[key], torch.Tensor):
+                res[key] = res[key].item()
+            else:
+                res[key] = float(res[key])
+
+        return res
+        
+    
+    def get_stft_metrics(self,mag_prd,mag_gt):
+        ## STFT related
+        mag_loss = torch.mean(torch.pow(mag_prd - mag_gt, 2)) * 2
+
+        return {
+                "audio_mag": mag_loss,
+        }
